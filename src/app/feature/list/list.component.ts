d1656650c9755a2467149115f0afb530654977e5
Matheus Kayque <matheuskayquebatistasilva712@gmail.com>
2024-07-13 21:17:24 -0300

    refactor: refatora listagem de produtos criando um componente card

--- src/app/feature/list/list.component.ts
+++ src/app/feature/list/list.component.ts
@@ -1,16 +1,14 @@
 import { HttpClient } from '@angular/common/http'
 import { Component, inject } from '@angular/core';
 
-import { MatCardModule } from '@angular/material/card'
-import { MatButtonModule } from '@angular/material/button'
-
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
