a728febbe35a144a1629b519b319906757f58139
Matheus Kayque <matheuskayquebatistasilva712@gmail.com>
2024-07-13 22:58:46 -0300

    feat: adiciona roteamento para o componente de criar novos produtos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 import { ListComponent } from './feature/list/list.component';
+import { CreateComponent } from './feature/create/create.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ListComponent
+  },
+  {
+    path: 'create-product',
+    component: CreateComponent
   }
 ];
