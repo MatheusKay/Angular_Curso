af3a5e767f00b81219b8f040ee33b7e95981ff6f
Matheus Kayque <matheuskayquebatistasilva712@gmail.com>
2024-07-14 00:52:54 -0300

    feat: adiciona a funcionalidade de salvar produtos novos

--- src/app/feature/create/create.component.ts
+++ src/app/feature/create/create.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button'
+import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-create',
@@ -18,6 +19,8 @@ import { MatButtonModule } from '@angular/material/button'
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+  productsService = inject(ProductsService)
+
   form = new FormGroup({
     title: new FormControl<string>('', {
       nonNullable: true,
@@ -26,6 +29,11 @@ export class CreateComponent {
   })
 
   onSubmit() {
-    this.form.controls.title.value;
+    this.productsService.post({
+      title: this.form.controls.title.value
+    })
+      .subscribe(() => {
+        alert('Publicado')
+      })
   }
 }
